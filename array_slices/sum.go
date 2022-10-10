e54b21228cf1942f706d3257209ed884ddde2e9c
Gabriel Barbosa Soares <gabriel.soares@saltpay.co>
2022-10-10 20:56:11 +0100

    first part of arrays

--- array_slices/sum.go
+++ array_slices/sum.go
@@ -1,6 +1,6 @@
 package array_slices
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	var sum int
 	//range returns two values - the index and the value
 	for _, number := range numbers {
